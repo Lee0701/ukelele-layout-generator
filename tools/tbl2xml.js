095ab327c3b200b23d5dd2e3f132af800b74f747
Lee0701 <hgl1002@naver.com>
2023-09-05 13:40:01 +0900

    Add complete layout generation

--- tools/tbl2xml.js
+++ tools/tbl2xml.js
@@ -6,8 +6,8 @@ const convert = (actions) => {
     Object.keys(actions).forEach((id) => {
         const set = new Set()
         result.push(`<action id="${id}">`)
-        for(action of actions[id]) {
-            const {state, next, output} = action
+        for(const [state, action] of Object.entries(actions[id])) {
+            const {next, output} = action
             if(output) result.push(`    <when state="${state}" output="${output}"/>`)
             else if(next) result.push(`    <when state="${state}" next="${next}"/>`)
             set.add(state)
@@ -17,19 +17,37 @@ const convert = (actions) => {
     return result.join('\n')
 }
 
+const format = (content) => {
+    const template = fs.readFileSync('tools/template.keylayout', 'utf-8')
+    const replacements = {
+        id: -Math.floor(Math.random() * 32768),
+        name: 'TUT-Code',
+        actions: content,
+    }
+    return template.replace(/\%([a-z]+)\%/g, (match, name) => replacements[name])
+}
+
 const main = async (inFile, outFile) => {
     const content = fs.readFileSync(inFile, 'utf-8')
-    const entries = content.split('\n').map((line) => line.split('\t'))
+    const entries = content.split(/\r?\n/).map((line) => line.split('\t'))
     const actions = {}
     entries.forEach(([key, value]) => {
         const id = key.charAt(key.length - 1)
         const state = key.slice(0, -1)
         const output = value.trim()
-        if(!actions[id]) actions[id] = []
-        actions[id].push({state, output})
+
+        if(!actions[id]) actions[id] = {}
+        actions[id][state] = {output}
+
+        const s = state.slice(0, -1) || 'none'
+        const i = state.charAt(state.length - 1)
+        const next = state
+        if(!actions[i]) actions[i] = {}
+        if(!actions[i][s]) actions[i][s] = {next}
     })
     const result = convert(actions)
-    fs.writeFileSync(outFile, result)
+
+    fs.writeFileSync(outFile, format(result))
 }
 
 if(require.main === module) main(...process.argv.slice(2))
